b28c4619c7f4a49882015bb191790fa3ddc7f07a
Diegudeveloper <diegudeveloper@gmail.com>
2022-02-25 11:41:43 +0100

    ejecucion del codigo en javascript.

--- 1Ejercicio/main.js
+++ 1Ejercicio/main.js
@@ -1,13 +1,45 @@
 const carrito = document.getElementById('carrito');
 const template = document.getElementById('template');
 const fragment = document.createDocumentFragment();
-const boton = document.querySelectorAll('btn');
+const boton = document.querySelectorAll('.card .btn');
+
+//console.log(boton);
 
 const objetoCarrito = {};
 
-const AgregarCarrito = (e) => {
-    console.log(e.target.dataset.fruta);
+const agregarCarrito = (e) => {
+    //console.log(e.target.dataset.fruta);
 
+    const producto = {
+        titulo: e.target.dataset.fruta,
+        id: e.target.dataset.fruta,
+        cantidad: 1
+    };
+
+    if(objetoCarrito.hasOwnProperty(producto.id)) {
+        producto.cantidad = objetoCarrito[producto.id].cantidad + 1;
+    }
+
+    objetoCarrito[producto.titulo] = producto;
+    //console.log(objetoCarrito);
+
+    pintarCarrito();
 }
 
-boton.forEach((e) => e.addEventListener('click', AgregarCarrito));
+boton.forEach((e) => e.addEventListener('click', agregarCarrito));
+
+const pintarCarrito = () => {
+
+    carrito.textContent = "";
+
+    Object.values(objetoCarrito).forEach((item) => {
+        const clone = template.content.cloneNode(true);
+        clone.querySelector('.lead').textContent = item.titulo;
+        clone.querySelector('.rounded-pill').textContent = item.cantidad;
+
+        fragment.appendChild(clone);
+    });
+
+    carrito.appendChild(fragment);
+}
+
